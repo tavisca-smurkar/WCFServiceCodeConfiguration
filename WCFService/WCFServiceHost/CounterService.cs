6868690997b6dff55701648bbcd71297debdd072
tavisca-smurkar <smurkar@tavisca.com>
2016-09-10 10:11:50 +0530

    initial commite=

---
+++ WCFService/WCFServiceHost/CounterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class CounterService : ICounterService
+    {
+        int counter = 0;
+        public int IncrementNumber()
+        {
+            return counter++;
+        }
+    }
+}
